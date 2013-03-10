2690aeb183960cd91c6c415b76314c926a9c3ad8
Gilad Shai <shai.gilad@gmail.com>
2013-03-10 12:58:20 -0400

    Parse the QR call into SRDonation

--- ScanForGood/SRFirstViewController.m
+++ ScanForGood/SRFirstViewController.m
@@ -8,11 +8,13 @@
 
 #import "SRFirstViewController.h"
 #import <ZXingObjC/ZXingObjC.h>
+#import "SRDonation.h"
 
 
 
 @interface SRFirstViewController ()
-
+- (void)decodeImage:(UIImage *)scanedImage;
+- (SRDonation *)parseQRResults:(ZXResult *)result;
 @end
 
 @implementation SRFirstViewController
@@ -40,36 +42,72 @@
 }
 
 - (IBAction)mainAction:(id)sender {
-    // Given a CGImage in which we are looking for barcodes
-    CGImageRef imageToDecode = [UIImage imageNamed:@"spottedRhino.png"].CGImage;
+    [self decodeImage:[UIImage imageNamed:@"spottedRhino.png"]];
+}
+
+#pragma mark - Processing scaned image
+
+- (void)decodeImage:(UIImage *)scanedImage {
     
-    ZXLuminanceSource* source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:imageToDecode];
-    ZXBinaryBitmap* bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
+    // Given a CGImage in which we are looking for barcodes
+    CGImageRef imageToDecode = scanedImage.CGImage;
+    
+    ZXLuminanceSource *source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:imageToDecode];
+    ZXBinaryBitmap *bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
     
     NSError* error = nil;
     
     // There are a number of hints we can give to the reader, including
     // possible formats, allowed lengths, and the string encoding.
-    ZXDecodeHints* hints = [ZXDecodeHints hints];
+    ZXDecodeHints *hints = [ZXDecodeHints hints];
     
-    ZXMultiFormatReader* reader = [ZXMultiFormatReader reader];
-    ZXResult* result = [reader decode:bitmap
+    ZXMultiFormatReader *reader = [ZXMultiFormatReader reader];
+    ZXResult *result = [reader decode:bitmap
                                 hints:hints
                                 error:&error];
     if (result) {
-        // The coded result as a string. The raw data can be accessed with
-        // result.rawBytes and result.length.
-        NSString* contents = result.text;
-        NSLog(@"contents: %@ \nMetadata: %@", contents, result.resultMetadata);
-        
-        // The barcode format, such as a QR code or UPC-A
-        ZXBarcodeFormat format = result.barcodeFormat;
-        NSLog(@"format: %d",format);
+        SRDonation *parsedResults = [self parseQRResults:result];
+        NSLog(@"parsed results: %@", parsedResults);
         
     } else {
         // Use error to determine why we didn't get a result, such as a barcode
         // not being found, an invalid checksum, or a format inconsistency.
         NSLog(@"Error");
     }
+}
+
+- (SRDonation *)parseQRResults:(ZXResult *)result {
+    /*
+     Testing data:
+     
+     {
+        "url": "http://www.spottedrhino.com",
+        "account_id": "23-11223344b",
+        "options": [
+            2,
+            3,
+            5
+        ]
+     }    
+     */
+    
+    // The coded result as a string. The raw data can be accessed with
+    // result.rawBytes and result.length.
+    NSError *jsonParsingError = nil;
+    NSData *data = [result.text dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *response = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonParsingError];
+
+    if (!response) {
+        return nil;
+    }
+    
+    // Build are Donation obj
+    SRDonation *donation = [[SRDonation alloc] initWithDictionary: response];
+    
+    // The barcode format, such as a QR code or UPC-A
+    ZXBarcodeFormat format = result.barcodeFormat;
+    NSLog(@"format: %d",format);
+    
+    return (SRDonation *)donation;
 }
 @end
